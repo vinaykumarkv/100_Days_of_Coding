d4fd618ab367c1f58ba9c0657249e05c22b65afc
vinaykumarkv <kvvk.win@gmail.com>
2023-08-31 23:32:50 +0100

    Day 4 folder

--- Day3code/TreasureIsland.py
+++ Day3code/TreasureIsland.py
@@ -26,16 +26,16 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 #we can add our own ascii art with https://ascii.co.uk/art
 #Write your code below this line 👇
-Direction = input("left or right?" )
+Direction = input("You are at cross road where do you want to go - left or right? " ).lower
 if Direction == "left":
-    Direction = input("swin or wait" )
+    Direction = input("You are at the Shore and there is an island in mid of lake do you want to swin or wait for boat? choose either wait or swim" ).lower
     if Direction == "wait":
-        Direction=input("Which door?" )
-        if Direction == "Yellow":
+        Direction=input("You are the castle Which door you want to select - Yellow, Red, Blue? " ).lower
+        if Direction == "yellow":
             print("You Win!")
-        elif Direction == "Red":
+        elif Direction == "red":
             print("Burned by fire.\nGame Over.")
-        elif Direction == "Blue":
+        elif Direction == "blue":
             print("Eaten by Beasts.\nGame Over.")
         else:
             print("Game Over.")

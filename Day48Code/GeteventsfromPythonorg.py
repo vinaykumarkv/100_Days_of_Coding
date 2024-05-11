5372f681c6b787c9da0221167d54944058bfca86
vinaykumarkv <kvvk.win@gmail.com>
2024-05-11 01:27:33 +0100

    Day 56 - learnt about developing websites using flask

--- Day48Code/GeteventsfromPythonorg.py
+++ Day48Code/GeteventsfromPythonorg.py
@@ -17,7 +17,7 @@ for i in range(len(temp_list)):
 		date_list.append(temp_list[i])
 	else:
 		event_name_list.append((temp_list[i]))
-#instead of above code we can also use findelements function to get tags based on CSS selector
+#instead of above code we can also use findelements function to get tags based on css selector
 dicts ={i:{date_list[i] : event_name_list[i]} for i in range(len(date_list))}
 print(dicts)
 driver.quit()

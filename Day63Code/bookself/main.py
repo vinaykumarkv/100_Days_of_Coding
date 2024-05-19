ca58c7eb80b3760b1028631a4ff0e5616271e676
vinaykumarkv <kvvk.win@gmail.com>
2024-05-19 23:01:01 +0100

    Day 63 - Worked on SQL Lite

---
+++ Day63Code/bookself/main.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+app = Flask(__name__)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    all_books = Book.query.all()
+    return render_template("index.html", books=all_books)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        rating = request.form['rating']
+        new_book = Book(title=title, author=author, rating=rating)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("add.html")
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    book = Book.query.get(id)
+    if request.method == 'POST':
+        book.rating = request.form['rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("update.html", book=book)
+
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    book = Book.query.get(id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
